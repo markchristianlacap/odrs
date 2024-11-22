93543c947ce9eb7d5d8e944fce07f8378af2471e
Mark Christian Lacap <markchristianlacap@gmail.com>
2024-11-22 17:17:39 +0800

    feat: contact us

--- client/pages/contact-us.vue
+++ client/pages/contact-us.vue
@@ -2,11 +2,19 @@
 
 <template>
   <div class="mx-auto mt-5xl container">
-    <p class="text-center text-xl font-bold">
+    <p class="mb-xl text-2xl font-bold">
+      <QIcon left class="text-4xl">
+        <div class="i-hugeicons:contact-book" />
+      </QIcon>
       Contact Us
     </p>
-    <p class="mt-xl text-primary font-bold">
-      Meet the team
-    </p>
+    <div class="space-y-sm">
+      <p class="mt-xl">
+        You can contact us here at our email address <b>omscsanjose_registrar@omsc.ph.education</b>
+      </p>
+      <p>Phone Number: (043) 457 2031 local: 135</p>
+      <p>Website: https://omsc.edu.ph</p>
+      <p>Address: Rizal Street,San Jose, Occidental Mindoro</p>
+    </div>
   </div>
 </template>
